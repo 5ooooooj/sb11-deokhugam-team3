4aa1afd9925e143a9f7a2ad9378cfbe25a2b7dfc
tndyd123 <wjdtndyd0001@gmail.com>
2026-05-21 17:30:10 +0900

    Refactor: BaseEntity에 UUID 식별자 추가

--- src/main/java/com/team3/deokhugam/global/entity/BaseEntity.java
+++ src/main/java/com/team3/deokhugam/global/entity/BaseEntity.java
@@ -2,8 +2,12 @@ package com.team3.deokhugam.global.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
+import java.util.UUID;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,6 +17,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id", nullable = false, updatable = false)
+  private UUID id;
 
   @CreatedDate
   @Column(name = "created_at", nullable = false, updatable = false)
