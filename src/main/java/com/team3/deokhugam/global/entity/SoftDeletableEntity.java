59a99ad3917cdc545029abbe4a697d1749c21c96
tndyd123 <wjdtndyd0001@gmail.com>
2026-05-21 16:55:02 +0900

    feat: BaseEntity와 SoftDeletableEntity 추가

---
+++ src/main/java/com/team3/deokhugam/global/entity/SoftDeletableEntity.java
@@ -0,0 +1,26 @@
+package com.team3.deokhugam.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public class SoftDeletableEntity extends BaseEntity{
+
+  @Column(name = "deleted_at")
+  private Instant deletedAt;
+
+  public void softDelete(){
+    this.deletedAt = Instant.now();
+  }
+
+  public void restore(){
+    this.deletedAt = null;
+  }
+
+  public boolean isDeleted() {
+    return deletedAt != null;
+  }
+}
