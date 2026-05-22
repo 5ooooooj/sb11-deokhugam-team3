b94d305076d9e6c2378aa6721832ec2fb26ac62b
tndyd123 <wjdtndyd0001@gmail.com>
2026-05-22 11:41:59 +0900

    Feat: UserRepository에 existsByEmail 추가

--- src/main/java/com/team3/deokhugam/domain/user/repository/UserRepository.java
+++ src/main/java/com/team3/deokhugam/domain/user/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.team3.deokhugam.domain.user.repository;
 
-public interface UserRepository {
+import com.team3.deokhugam.domain.user.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface UserRepository extends JpaRepository<User, UUID> {
+  boolean existsByEmail(String email);
 }
