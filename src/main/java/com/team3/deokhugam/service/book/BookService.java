b4f796ee7affac7b8d13612e1b4f8862e89abaa5
강우진 <spacein1001@gmail.com>
2026-05-26 10:25:58 +0900

    Feat: 도서 생성 Service 구현 [#12]

---
+++ src/main/java/com/team3/deokhugam/service/book/BookService.java
@@ -0,0 +1,32 @@
+package com.team3.deokhugam.service.book;
+
+import com.team3.deokhugam.domain.book.Book;
+import com.team3.deokhugam.dto.book.BookCreateRequest;
+import com.team3.deokhugam.dto.book.BookDto;
+import com.team3.deokhugam.repository.book.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+  private final BookRepository bookRepository;
+
+  public BookDto create(BookCreateRequest request) {
+
+    Book book =
+        new Book(
+            request.title(),
+            request.author(),
+            request.description(),
+            request.publisher(),
+            request.publishedDate(),
+            request.isbn(),
+            request.thumbnailUrl()
+        );
+    Book savedBook = bookRepository.save(book);
+
+    return BookDto.from(savedBook);
+  }
+}
