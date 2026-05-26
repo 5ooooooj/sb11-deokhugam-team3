5f5fce67ac63d4ba8c9f4eb5559f421e58e51fbf
JG <siz.jg00@gmail.com>
2026-05-26 16:18:26 +0900

    Fix: Comment 엔티티 @Entity 어노테이션 추가

--- src/main/java/com/team3/deokhugam/domain/comment/Comment.java
+++ src/main/java/com/team3/deokhugam/domain/comment/Comment.java
@@ -1,6 +1,10 @@
 package com.team3.deokhugam.domain.comment;
 
 import com.team3.deokhugam.global.entity.SoftDeletableEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "comments")
 public class Comment extends SoftDeletableEntity {
 }
