295942bb96a00c221cd70dbdbb6f9a1645ebf61e
Yunsun <justina7182@gmail.com>
2026-05-26 11:41:05 +0900

    Feat: aws s3 config 작성

---
+++ src/main/java/com/team3/deokhugam/global/config/AwsProperties.java
@@ -0,0 +1,22 @@
+package com.team3.deokhugam.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "aws")
+@Getter
+@Setter
+public class AwsProperties {
+
+    private String region;
+    private S3 s3 = new S3();
+
+    @Getter
+    @Setter
+    public static class S3 {
+      private String bucket;
+    }
+}
