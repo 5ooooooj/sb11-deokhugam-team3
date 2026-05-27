a00383a54260bb637f63820341298ae78e752373
tndyd123 <wjdtndyd0001@gmail.com>
2026-05-27 16:07:31 +0900

    feat: UserController 구현

--- src/main/java/com/team3/deokhugam/controller/user/UserController.java
+++ src/main/java/com/team3/deokhugam/controller/user/UserController.java
@@ -2,8 +2,10 @@ package com.team3.deokhugam.controller.user;
 
 import com.team3.deokhugam.controller.user.docs.UserFindByIdApi;
 import com.team3.deokhugam.controller.user.docs.UserRegisterApi;
+import com.team3.deokhugam.controller.user.docs.UserUpdateApi;
 import com.team3.deokhugam.dto.user.UserRegisterRequest;
 import com.team3.deokhugam.dto.user.UserDto;
+import com.team3.deokhugam.dto.user.UserUpdateRequest;
 import com.team3.deokhugam.service.user.UserService;
 import com.team3.deokhugam.controller.user.docs.UserLoginApi;
 import com.team3.deokhugam.dto.user.UserLoginRequest;
@@ -14,9 +16,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,8 +52,17 @@ public class UserController {
 
   @UserFindByIdApi
   @GetMapping("/{userId}")
-  public ResponseEntity<UserDto> findUserById(@PathVariable UUID userId){
+  public ResponseEntity<UserDto> findUserById(@PathVariable UUID userId) {
     UserDto response = userService.findUserById(userId);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
+
+  @UserUpdateApi
+  @PatchMapping("/{userId}")
+  public ResponseEntity<UserDto> updateUser(@PathVariable UUID userId,
+      @RequestHeader("Deokhugam-Request-User-ID") UUID loginUserId,
+      @Valid @RequestBody UserUpdateRequest request) {
+    UserDto response = userService.updateUser(userId, loginUserId, request);
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 }
