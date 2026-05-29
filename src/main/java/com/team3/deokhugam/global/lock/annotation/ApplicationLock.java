90b58a1443ee794be245242ce6aee5834865c529
tndyd123 <wjdtndyd0001@gmail.com>
2026-05-29 17:37:41 +0900

    feat: Lock전용 인터페이스 구현

--- src/main/java/com/team3/deokhugam/global/lock/annotation/ApplicationLock.java
+++ src/main/java/com/team3/deokhugam/global/lock/annotation/ApplicationLock.java
@@ -1,5 +1,16 @@
 package com.team3.deokhugam.global.lock.annotation;
 
+import com.team3.deokhugam.global.lock.domain.LockName;
+import com.team3.deokhugam.global.lock.domain.LockTarget;
+
 public @interface ApplicationLock {
+
+  LockTarget target();
+
+  String targetId();
+
+  LockName lockName();
+
+  String duration() default "3s";
 
 }
