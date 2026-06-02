9248be83db00488d4ae330d4e81ca79f345bf215
Yunsun <justina7182@gmail.com>
2026-06-02 16:09:18 +0900

    Chore: 불필요 import 제거

--- src/test/java/com/team3/deokhugam/batch/step/listener/PopularBookRankingListenerTest.java
+++ src/test/java/com/team3/deokhugam/batch/step/listener/PopularBookRankingListenerTest.java
@@ -2,7 +2,6 @@ package com.team3.deokhugam.batch.step.listener;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
