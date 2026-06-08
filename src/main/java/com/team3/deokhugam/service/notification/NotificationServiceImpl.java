a9fcc56003b942db6d12f8bf85e89b7064709bc7
Yunsun <justina7182@gmail.com>
2026-06-08 16:24:59 +0900

    Test: 알림 연동 관련 서비스 테스트 코드 추가

--- src/main/java/com/team3/deokhugam/service/notification/NotificationServiceImpl.java
+++ src/main/java/com/team3/deokhugam/service/notification/NotificationServiceImpl.java
@@ -40,7 +40,7 @@ public class NotificationServiceImpl implements NotificationService {
 
   @Override
   public void createRankingNotification(UUID reviewId, String period) {
-    String message = "리뷰가 " + period + "TOP10에 선정되었습니다.";
+    String message = "리뷰가 " + period + " TOP10에 선정되었습니다.";
 
     if (notificationRepository.existsByReviewIdAndTypeAndMessage(reviewId, NotificationType.POPULAR_REVIEW, message)) {
       log.debug("이미 랭킹 알림이 존재합니다. reviewId: {}", reviewId);
